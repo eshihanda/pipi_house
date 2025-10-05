b3243a2d0695201b7b6fc92ac8466e1aad55a82c
lupamo <arnoldlupamo@gmail.com>
2025-10-05 13:32:33 +0300

    cards

--- Frontend/pipi_eastafrica/src/App.jsx
+++ Frontend/pipi_eastafrica/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import TopNavbar from './Components/TopNavbar'
 import Faq from './Components/Faq'
 import Footer from './Components/Footer'
-
+import ProductExplorer from './Components/product-explorer'
 const App = () => {
   return (
     <div>
       <TopNavbar />
+      <ProductExplorer />
       <Faq />
       <Footer />
     </div>
